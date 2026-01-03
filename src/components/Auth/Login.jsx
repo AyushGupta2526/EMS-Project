41c2979dbda4798d1b5387a16995fb46874318a5
Ayush Gupta <guptaayush2526@gmail.com>
2026-01-03 17:55:41 +0530

    fixing login issues

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -25,14 +25,14 @@ const Login = ({ handleLogin }) => {
             onChange={(e)=>{
                 setEmail(e.target.value)
             }}
-            required className='outline-none bg-transparent border-2 border-emerald-600 rounded-full text-xl py-3 px-5 placeholder:text-gray-400 ' type="Email" placeholder='Enter your email' />
+            required className='outline-none bg-transparent border-2 border-emerald-600 rounded-full text-xl py-3 px-5 placeholder:text-gray-400 ' type="email" placeholder='Enter your email' />
             <input 
             value={password}
             onChange={(e)=>{
                 setPassword(e.target.value)
             }}
-            required className='outline-none bg-transparent border-2 border-emerald-600 rounded-full text-xl py-3 px-5 placeholder:text-gray-400 mt-3' type="Password" placeholder='Enter password' />
-            <button className='mt-5 text-white border-none outline-none border-2 bg-emerald-600 rounded-full text-xl py-3 px-5 '>Log in</button>
+            required className='outline-none bg-transparent border-2 border-emerald-600 rounded-full text-xl py-3 px-5 placeholder:text-gray-400 mt-3' type="password" placeholder='Enter password' />
+            <button className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
         </form>
      </div>
     </div>
